e18569a9a71ed977f5e2f7da3d136a6dab6845cd
dale-20 <laureto.christiandale22@example.com>
2026-01-22 22:49:44 +0800

    final..

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     {
         $projects = [
             ['title' => 'Tower Bound', 'description' => 'A text_adventure game',  'image' => 'tower_bound_icon.png', 'tools'=> ['Java', 'Android Studio', 'SQLite']],
-            ['title' => 'Pet Care', 'description' => 'An infomation management system for a veterinary clinic', 'image' => 'pet_care_icon.png', 'tools'=> ['PHP', 'Bootstrap' ,'MySQL']],
+            ['title' => 'Pet Care', 'description' => 'A web-based infomation management system for a veterinary clinic', 'image' => 'pet_care_icon.png', 'tools'=> ['PHP', 'Bootstrap' ,'MySQL']],
             ['title' => 'TrackMyRun', 'description' => 'A mobile run traker application', 'image' => 'run_logo.png', 'tools'=> ['Java', 'Android Studio', 'MySQL']],
             ['title' => 'Smart Budget', 'description' => 'An expense traking application for mobile', 'image' => 'smart_budget_icon.png', 'tools'=> ['Flutter', 'Dart', 'SQLite']],
         ];
